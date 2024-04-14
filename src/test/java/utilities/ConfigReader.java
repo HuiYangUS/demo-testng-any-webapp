ee9d11dd91de3f5c6460169e0288d8c97f1c1502
Hui Yang <hui.yang.9045@gmail.com>
2024-04-14 11:54:10 -0700

    minor tweak

--- src/test/java/utilities/ConfigReader.java
+++ src/test/java/utilities/ConfigReader.java
@@ -6,21 +6,21 @@ import java.util.Properties;
 
 public class ConfigReader {
 
-    private static final String FILE_PATH = "src/test/resources/configs/config.properties";
-    private static Properties p;
+	private static final String FILE_PATH = "src/test/resources/configs/config.properties";
+	private static Properties p;
 
-    public static String getTextValue(String key){
-        loadPropertiesFile();
-        return p.getProperty(key);
-    }
+	public static String getTextValue(String key) {
+		loadPropertiesFile();
+		return p.getProperty(key);
+	}
 
-    public static void loadPropertiesFile(){
-        p=new Properties();
-        try {
-            p.load(new FileInputStream(FILE_PATH));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	public static void loadPropertiesFile() {
+		p = new Properties();
+		try {
+			p.load(new FileInputStream(FILE_PATH));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
